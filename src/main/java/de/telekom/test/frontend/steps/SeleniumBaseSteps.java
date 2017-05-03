787dc0359d9882f27b003f9f03e9d03e71acb417
t.joedicke <t.joedicke@telekom.de>
2017-05-03 13:52:02 +0200

    #bestefehlermeldung

--- src/main/java/de/telekom/test/frontend/steps/SeleniumBaseSteps.java
+++ src/main/java/de/telekom/test/frontend/steps/SeleniumBaseSteps.java
@@ -14,21 +14,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 @Steps
 public final class SeleniumBaseSteps extends SeleniumSteps {
 
-	@When("run browserback")
-	public void runBrowserback() {
-		webDriverWrapper.getDriver().navigate().back();
-	}
+    @When("run browserback")
+    public void runBrowserback() {
+        webDriverWrapper.getDriver().navigate().back();
+    }
 
-	@When("run browserforward")
-	public void runBrowserforward() {
-		webDriverWrapper.getDriver().navigate().forward();
-	}
+    @When("run browserforward")
+    public void runBrowserforward() {
+        webDriverWrapper.getDriver().navigate().forward();
+    }
 
-	@Then("the page with url $url is shown")
-	public void thePageWithUrlIsShown(String url) {
-		WebDriver driver = webDriverWrapper.getDriver();
-		Wait<WebDriver> wait = new WebDriverWait(driver, 30);
-		wait.until(new UrlMatchesExpectation(driver, url, "The url " + url + " is not shown!"));
-	}
+    @Then("the page with url $url is shown")
+    public void thePageWithUrlIsShown(String url) {
+        WebDriver driver = webDriverWrapper.getDriver();
+        Wait<WebDriver> wait = new WebDriverWait(driver, 30);
+        wait.until(new UrlMatchesExpectation(driver, url, String.format("The url %s is not shown! (was: %s)", url, driver.getCurrentUrl())));
+    }
 
 }
