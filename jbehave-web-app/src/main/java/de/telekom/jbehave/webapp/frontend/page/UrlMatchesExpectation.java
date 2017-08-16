8275f49638adb2ac2d14ad18dc690264cd956f99
d.keiss <daniel.keiss@telekom.de>
2017-08-16 11:49:55 +0200

    The verification of the current url when creating the page object can now also be done with a regular expression

--- jbehave-web-app/src/main/java/de/telekom/jbehave/webapp/frontend/page/UrlMatchesExpectation.java
+++ jbehave-web-app/src/main/java/de/telekom/jbehave/webapp/frontend/page/UrlMatchesExpectation.java
@@ -3,8 +3,6 @@ package de.telekom.jbehave.webapp.frontend.page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
-import static org.apache.commons.lang3.StringUtils.contains;
-
 /**
  * ExpectedCondition for url matching.
  *
@@ -24,7 +22,8 @@ public class UrlMatchesExpectation implements ExpectedCondition<Boolean> {
 
     @Override
     public Boolean apply(WebDriver driver) {
-        return contains(driver.getCurrentUrl(), urlToMatch);
+        String regex = ".*" + urlToMatch + ".*"; // contains
+        return driver.getCurrentUrl().matches(regex);
     }
 
     @Override
