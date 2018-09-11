d39b716f71ec8aebab2055a6e0cd3558b7707531
d.keiss <daniel.keiss@telekom.de>
2018-09-11 13:42:32 +0200

    Extract german keyword configuration

--- bdd-web-app-demo/taxi-test-ger/src/test/java/de/telekom/test/bddwebapp/taxi/config/AbstractTaxiStory.java
+++ bdd-web-app-demo/taxi-test-ger/src/test/java/de/telekom/test/bddwebapp/taxi/config/AbstractTaxiStory.java
@@ -2,20 +2,14 @@ package de.telekom.test.bddwebapp.taxi.config;
 
 import de.telekom.test.bddwebapp.stories.AbstractStory;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.configuration.Keywords;
-import org.jbehave.core.i18n.LocalizedKeywords;
-import org.jbehave.core.parsers.RegexStoryParser;
-import org.jbehave.core.steps.MarkUnmatchedStepsAsPending;
 import org.springframework.context.ApplicationContext;
-
-import java.util.Locale;
 
 /**
  * @author Daniel Keiss
  * <p>
  * Copyright (c) 2017 Daniel Keiss, Deutsche Telekom AG
  */
-public abstract class AbstractTaxiStory extends AbstractStory {
+public abstract class AbstractTaxiStory extends AbstractStory implements GermanKeywordsConfiguration {
 
     @Override
     public ApplicationContext getApplicationContext() {
@@ -24,15 +18,7 @@ public abstract class AbstractTaxiStory extends AbstractStory {
 
     @Override
     public Configuration configuration() {
-        // use german keywords
-        Locale de = new Locale("de");
-        Keywords keywords = new LocalizedKeywords(de);
-        Configuration configuration = defaultConfiguration();
-        configuration.useKeywords(keywords);
-        configuration.useStepCollector(new MarkUnmatchedStepsAsPending(keywords));
-        configuration.useStoryReporterBuilder(defaultStoryReporterBuilder().withKeywords(keywords));
-        configuration.useStoryParser(new RegexStoryParser(configuration));
-        return configuration;
+        return germanKeywordsConfiguration();
     }
 
 }
