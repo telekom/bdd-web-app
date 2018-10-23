ab49d3ab3a6995a6021f91f993eb5216f58d0d5a
d.keiss <daniel.keiss@telekom.de>
2018-10-23 12:26:24 +0200

    Renamed integration project to customizing

---
+++ bdd-web-app-demo/taxi-test-customzing/src/test/java/de/telekom/test/bddwebapp/taxi/customizing/stories/api/CheckUserFixture.java
@@ -0,0 +1,15 @@
+package de.telekom.test.bddwebapp.taxi.customizing.stories.api;
+
+import de.telekom.test.bddwebapp.api.ApiOnly;
+import de.telekom.test.bddwebapp.taxi.customizing.config.AbstractTestLevelStory;
+
+/**
+ * @author Daniel Keiss {@literal <daniel.keiss@telekom.de>}
+ * <p>
+ * Copyright (c) 2018 Daniel Keiss, Deutsche Telekom AG
+ * This file is distributed under the conditions of the Apache License, Version 2.0.
+ * For details see the file license on the toplevel.
+ */
+@ApiOnly
+public class CheckUserFixture extends AbstractTestLevelStory {
+}
