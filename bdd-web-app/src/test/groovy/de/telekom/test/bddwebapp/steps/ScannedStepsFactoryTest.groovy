b84217b92a1b1e25c163cf2ab18165fa669b0f3a
d.keiss <daniel.keiss@telekom.de>
2018-11-07 17:38:00 +0100

    Refactored test level steps factory

--- bdd-web-app/src/test/groovy/de/telekom/test/bddwebapp/steps/ScannedStepsFactoryTest.groovy
+++ bdd-web-app/src/test/groovy/de/telekom/test/bddwebapp/steps/ScannedStepsFactoryTest.groovy
@@ -10,6 +10,7 @@ class ScannedStepsFactoryTest extends Specification {
     def applicationContextMock = Mock(ApplicationContext.class)
     def configurationMock = Mock(Configuration.class)
     def testStepLevel0 = new TestLevel0Step();
+    def testStepLevel1 = new TestLevel1Step();
     def testLevel1ExtendsTestLevel0Step = new TestLevel1ExtendsTestLevel0Step();
 
     ScannedStepsFactory scannedStepsFactory = new ScannedStepsFactory() {
@@ -35,8 +36,9 @@ class ScannedStepsFactoryTest extends Specification {
     def "run test level 0 with steps on different test level"() {
         given:
         scannedStepsFactory.applicationContext.getBeansWithAnnotation(Steps.class) >>
-                ["testLevel0": testStepLevel0]
-        ["testLevel1": testLevel1ExtendsTestLevel0Step]
+                ["testLevel0"        : testStepLevel0,
+                 "testLevel1"        : testStepLevel1,
+                 "testLevel1Extended": testLevel1ExtendsTestLevel0Step]
         when:
         InstanceStepsFactory factory = scannedStepsFactory.testLevelStepsFactory(0)
         then:
@@ -46,22 +48,36 @@ class ScannedStepsFactoryTest extends Specification {
     def "run test level 1 with test level 1 steps only"() {
         given:
         scannedStepsFactory.applicationContext.getBeansWithAnnotation(Steps.class) >>
-                ["testLevel1": testLevel1ExtendsTestLevel0Step]
+                ["testLevel1"        : testStepLevel1,
+                 "testLevel1Extended": testLevel1ExtendsTestLevel0Step]
         when:
         InstanceStepsFactory factory = scannedStepsFactory.testLevelStepsFactory(1)
         then:
-        factory.stepsInstances.values().asList() == [testLevel1ExtendsTestLevel0Step]
+        factory.stepsInstances.values().asList() == [testStepLevel1, testLevel1ExtendsTestLevel0Step]
     }
 
     def "run test level 1 with steps on different test level"() {
         given:
         scannedStepsFactory.applicationContext.getBeansWithAnnotation(Steps.class) >>
-                ["testLevel0": testStepLevel0,
-                 "testLevel1": testLevel1ExtendsTestLevel0Step]
+                ["testLevel0"        : testStepLevel0,
+                 "testLevel1"        : testStepLevel1,
+                 "testLevel1Extended": testLevel1ExtendsTestLevel0Step]
         when:
         InstanceStepsFactory factory = scannedStepsFactory.testLevelStepsFactory(1)
         then:
-        factory.stepsInstances.values().asList() == [testLevel1ExtendsTestLevel0Step]
+        factory.stepsInstances.values().asList() == [testStepLevel1, testLevel1ExtendsTestLevel0Step]
+    }
+
+    def "run test level 1 with steps on different test level with different order"() {
+        given:
+        scannedStepsFactory.applicationContext.getBeansWithAnnotation(Steps.class) >>
+                ["testLevel1Extended": testLevel1ExtendsTestLevel0Step,
+                 "testLevel0"        : testStepLevel0,
+                 "testLevel1"        : testStepLevel1]
+        when:
+        InstanceStepsFactory factory = scannedStepsFactory.testLevelStepsFactory(1)
+        then:
+        factory.stepsInstances.values().asList() == [testLevel1ExtendsTestLevel0Step, testStepLevel1]
     }
 
 
---
+++ bdd-web-app/src/test/groovy/de/telekom/test/bddwebapp/steps/TestLevel1Step.java
@@ -0,0 +1,5 @@
+package de.telekom.test.bddwebapp.steps;
+
+@Steps(testLevel = 1)
+public class TestLevel1Step {
+}
