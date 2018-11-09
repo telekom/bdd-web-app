f546d9897f238b0e9b200cf04d68864c77adf2b9
d.keiss <daniel.keiss@telekom.de>
2018-11-09 14:25:11 +0100

    Deploy taxi webapp as war

--- bdd-web-app-demo/taxi/src/main/java/de/telekom/test/bddwebapp/Application.java
+++ bdd-web-app-demo/taxi/src/main/java/de/telekom/test/bddwebapp/Application.java
@@ -4,8 +4,10 @@ import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,10 +21,15 @@ import java.sql.SQLException;
  * For details see the file license on the toplevel.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @Bean
