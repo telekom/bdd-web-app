2b89d25b5dfaa3f51f34ed6ca2218045da392caf
d.keiss <daniel.keiss@telekom.de>
2019-05-02 21:40:50 +0200

    Removed requestBuilder

--- demo/test/taxi-test-customizing/src/test/java/de/telekom/test/bddwebapp/taxi/customizing/steps/RegistrationStepsTestLevel.java
+++ demo/test/taxi-test-customizing/src/test/java/de/telekom/test/bddwebapp/taxi/customizing/steps/RegistrationStepsTestLevel.java
@@ -1,13 +1,9 @@
 package de.telekom.test.bddwebapp.taxi.customizing.steps;
 
-import de.telekom.test.bddwebapp.api.RequestBuilder;
-import de.telekom.test.bddwebapp.interaction.StoryInteraction;
 import de.telekom.test.bddwebapp.steps.Steps;
 import de.telekom.test.bddwebapp.taxi.steps.RegistrationSteps;
+import lombok.extern.slf4j.Slf4j;
 import org.jbehave.core.annotations.Given;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
@@ -18,20 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  * For details see the file license on the toplevel.
  */
 @Steps(testLevel = 1)
+@Slf4j
 public class RegistrationStepsTestLevel extends RegistrationSteps {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Autowired
-    private StoryInteraction storyInteraction;
-
-    @Autowired
-    private RequestBuilder requestBuilder;
 
     @Override
     @Given("registered user as $testobject")
     public void registeredUser(String testobject) {
-        logger.info("Create registered user in \"real-system\"");
+        log.info("Create registered user in \"real-system\"");
         // In the real scenario you wouldn't run the super method.
         // You would use an own implementation that create the user in the real system.
         super.registeredUser(testobject);
