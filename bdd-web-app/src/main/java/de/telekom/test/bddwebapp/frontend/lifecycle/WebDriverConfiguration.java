5efa62dbd9ab643eb427a5ef982298f754b8c978
d.keiss <daniel.keiss@telekom.de>
2019-11-08 15:04:26 +0100

    Adjusted the default configurations

--- bdd-web-app/src/main/java/de/telekom/test/bddwebapp/frontend/lifecycle/WebDriverConfiguration.java
+++ bdd-web-app/src/main/java/de/telekom/test/bddwebapp/frontend/lifecycle/WebDriverConfiguration.java
@@ -34,54 +34,67 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
  */
 public interface WebDriverConfiguration {
 
-    default DesiredCapabilities capabilities(String browser) {
-        return new DesiredCapabilities();
-    }
-
     default WebDriver loadWebdriver() {
         String browser = getBrowser();
         LoggerFactory.getLogger(WebDriverConfiguration.class).info("Browser is set to: " + browser);
-        DesiredCapabilities capabilities = capabilities(browser);
+        DesiredCapabilities extraCapabilities = extraCapabilities(browser);
         if (isRemoteWebdriver()) {
-            return loadRemoteWebdriver(capabilities);
+            return loadRemoteWebdriver(extraCapabilities);
         }
         switch (browser.toLowerCase()) {
             case "firefox":
-                return loadFirefox(capabilities);
+                return loadFirefox(extraCapabilities);
             case "chrome":
-                return loadChrome(capabilities);
+                return loadChrome(extraCapabilities);
             case "edge":
-                return loadEdge(capabilities);
+                return loadEdge(extraCapabilities);
             case "ie":
             case "internetexplorer":
-                return loadInternetExplorer(capabilities);
+                return loadInternetExplorer(extraCapabilities);
             case "opera":
-                return loadOpera(capabilities);
+                return loadOpera(extraCapabilities);
             case "safari":
-                return loadSafari(capabilities);
+                return loadSafari(extraCapabilities);
             case "htmlunit":
-                return loadHtmlUnit(capabilities);
+                return loadHtmlUnit(extraCapabilities);
             default:
                 throw new IllegalArgumentException("No browser defined! Given browser is: " + browser);
         }
+    }
+
+    default DesiredCapabilities extraCapabilities(String browser) {
+        return new DesiredCapabilities();
     }
 
     default boolean isRemoteWebdriver() {
         return isNotBlank(getGridURL());
     }
 
+    default DesiredCapabilities remoteWebDriverOptions(DesiredCapabilities capabilities) {
+        DesiredCapabilities remoteWebDriverCapabilities = new DesiredCapabilities();
+        remoteWebDriverCapabilities.setJavascriptEnabled(true);
+        remoteWebDriverCapabilities.merge(capabilities);
+        return capabilities;
+    }
+
     default WebDriver loadRemoteWebdriver(DesiredCapabilities capabilities) {
         String gridURL = getGridURL();
         LoggerFactory.getLogger(WebDriverConfiguration.class).info("Running on: " + gridURL);
         try {
-            return new RemoteWebDriver(new URL(gridURL), capabilities);
+            return new RemoteWebDriver(new URL(gridURL), remoteWebDriverOptions(capabilities));
         } catch (MalformedURLException e) {
             throw new IllegalArgumentException("URL for remote webdriver is malformed!", e);
         }
     }
 
+    default FirefoxOptions firefoxOptions(DesiredCapabilities capabilities) {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.merge(capabilities);
+        return firefoxOptions;
+    }
+
     default WebDriver loadFirefox(DesiredCapabilities capabilities) {
-        FirefoxOptions firefoxOptions = new FirefoxOptions(capabilities);
+        FirefoxOptions firefoxOptions = firefoxOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             LoggerFactory.getLogger(WebDriverConfiguration.class).info("Load portable firefox instance from '{}'", browserPath);
@@ -90,9 +103,14 @@ public interface WebDriverConfiguration {
         return new FirefoxDriver(firefoxOptions);
     }
 
-    default WebDriver loadChrome(DesiredCapabilities capabilities) {
+    default ChromeOptions chromeOptions(DesiredCapabilities capabilities) {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.merge(capabilities);
+        return chromeOptions;
+    }
+
+    default WebDriver loadChrome(DesiredCapabilities capabilities) {
+        ChromeOptions chromeOptions = chromeOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             LoggerFactory.getLogger(WebDriverConfiguration.class).info("Load portable chrome instance from '{}'", browserPath);
@@ -101,9 +119,14 @@ public interface WebDriverConfiguration {
         return new ChromeDriver(chromeOptions);
     }
 
-    default WebDriver loadEdge(DesiredCapabilities capabilities) {
+    default EdgeOptions edgeOptions(DesiredCapabilities capabilities) {
         EdgeOptions edgeOptions = new EdgeOptions();
         edgeOptions.merge(capabilities);
+        return edgeOptions;
+    }
+
+    default WebDriver loadEdge(DesiredCapabilities capabilities) {
+        EdgeOptions edgeOptions = edgeOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             throw new IllegalArgumentException("Can't use 'browserPath' for edge browser. Portable is not supported!");
@@ -111,8 +134,14 @@ public interface WebDriverConfiguration {
         return new EdgeDriver(edgeOptions);
     }
 
+    default InternetExplorerOptions internetExplorerOptions(DesiredCapabilities capabilities) {
+        InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+        internetExplorerOptions.merge(capabilities);
+        return internetExplorerOptions;
+    }
+
     default WebDriver loadInternetExplorer(DesiredCapabilities capabilities) {
-        InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions(capabilities);
+        InternetExplorerOptions internetExplorerOptions = internetExplorerOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             throw new IllegalArgumentException("Can't use 'browserPath' for Internet Explorer. Portable is not supported!");
@@ -120,9 +149,14 @@ public interface WebDriverConfiguration {
         return new InternetExplorerDriver(internetExplorerOptions);
     }
 
-    default WebDriver loadOpera(DesiredCapabilities capabilities) {
+    default OperaOptions operaOptions(DesiredCapabilities capabilities) {
         OperaOptions operaOptions = new OperaOptions();
         operaOptions.merge(capabilities);
+        return operaOptions;
+    }
+
+    default WebDriver loadOpera(DesiredCapabilities capabilities) {
+        OperaOptions operaOptions = operaOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             LoggerFactory.getLogger(WebDriverConfiguration.class).info("Load portable opera instance from '{}'", browserPath);
@@ -131,9 +165,14 @@ public interface WebDriverConfiguration {
         return new OperaDriver(operaOptions);
     }
 
-    default WebDriver loadSafari(DesiredCapabilities capabilities) {
+    default SafariOptions safariOptions(DesiredCapabilities capabilities) {
         SafariOptions safariOptions = new SafariOptions();
         safariOptions.merge(capabilities);
+        return safariOptions;
+    }
+
+    default WebDriver loadSafari(DesiredCapabilities capabilities) {
+        SafariOptions safariOptions = safariOptions(capabilities);
         String browserPath = getBrowserPath();
         if (isNotBlank(browserPath)) {
             throw new IllegalArgumentException("Can't use 'browserPath' for Safari. Portable is not supported!");
@@ -141,10 +180,15 @@ public interface WebDriverConfiguration {
         return new SafariDriver(safariOptions);
     }
 
+    default DesiredCapabilities htmlUnitOptions(DesiredCapabilities capabilities) {
+        DesiredCapabilities htmlUnitCapabilities = new DesiredCapabilities();
+        htmlUnitCapabilities.setJavascriptEnabled(true);
+        htmlUnitCapabilities.merge(capabilities);
+        return capabilities;
+    }
+
     default WebDriver loadHtmlUnit(DesiredCapabilities capabilities) {
-        HtmlUnitDriver htmlUnitDriver = new HtmlUnitDriver(capabilities);
-        htmlUnitDriver.setJavascriptEnabled(true);
-        return htmlUnitDriver;
+        return new HtmlUnitDriver(htmlUnitOptions(capabilities));
     }
 
     default void afterLoad(WebDriver driver) {
