81aa46c91d040cd061eb2809cc1ed18560fac10a
d.keiss <daniel.keiss@telekom.de>
2020-02-21 14:17:34 +0100

    Revert "Experimental function: Reload webdriver after crash. Rethrow exception"
    This reverts commit 5be187de

--- bdd-web-app/src/main/java/de/telekom/test/bddwebapp/frontend/lifecycle/WebDriverWrapper.java
+++ bdd-web-app/src/main/java/de/telekom/test/bddwebapp/frontend/lifecycle/WebDriverWrapper.java
@@ -84,7 +84,6 @@ public class WebDriverWrapper {
     public void loadWebdriver() {
         WebDriverConfiguration webDriverConfiguration = getCurrentWebDriverConfiguration();
 
-        WebDriverException lastException = new WebDriverException();
         for (int i = 0; i < 3; i++) {
             try {
                 WebDriver webDriver = webDriverConfiguration.loadWebdriver();
@@ -98,11 +97,9 @@ public class WebDriverWrapper {
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
-                lastException = e;
             }
         }
-        log.error("Too many tries loading webdriver! Rethrow webdriver exception.");
-        throw lastException;
+        log.error("Too many tries loading webdriver!");
     }
 
     public void quit() {
