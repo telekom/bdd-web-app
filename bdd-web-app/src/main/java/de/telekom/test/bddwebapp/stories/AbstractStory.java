49b752322c41cf6ed5a3b3a94732b5ed68c2dd59
d.keiss <daniel.keiss@telekom.de>
2020-05-08 16:12:23 +0200

    Added support for meta filter passing at story execution. Added the possibility to execute single scenarios.

--- bdd-web-app/src/main/java/de/telekom/test/bddwebapp/stories/AbstractStory.java
+++ bdd-web-app/src/main/java/de/telekom/test/bddwebapp/stories/AbstractStory.java
@@ -8,10 +8,15 @@ import de.telekom.test.bddwebapp.stories.customizing.CurrentStoryEmbedderMonitor
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.embedder.MetaFilter;
 import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.model.Meta;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
@@ -50,10 +55,16 @@ public abstract class AbstractStory extends JUnitStory implements ScannedStepsFa
         Embedder embedder = super.configuredEmbedder();
         embedder.useEmbedderMonitor(new CurrentStoryEmbedderMonitor(getApplicationContext()));
 
-        if (isExecutedByJUnitRunner()) {
-            // deactivate view generation for single story runs to prevent false positive
+        // deactivate view generation for single story runs to prevent false positive
+        if(isExecutedByJUnitRunner()){
             embedder.embedderControls().doGenerateViewAfterStories(false);
         }
+
+        // adding meta filter support for -DmetaFilters=...
+        metaFilters().ifPresent(metaFilters -> {
+            embedder.useMetaFilters(Arrays.asList(metaFilters.split(";")));
+            supportForSingleScenarioExecution(embedder);
+        });
 
         return embedder;
     }
@@ -62,6 +73,36 @@ public abstract class AbstractStory extends JUnitStory implements ScannedStepsFa
         // the test class here is a indicator that the story is run by maven build and not by junit-class
         String testClass = System.getProperty("test");
         return isBlank(testClass);
+    }
+
+    public Optional<String> metaFilters() {
+        String metaFilters = System.getProperty("metaFilters");
+        return Optional.ofNullable(metaFilters);
+    }
+
+    /**
+     * Adding meta matcher for execution of single scenarios, e.g. with the intellij plugin "jbehave debug single scenario"
+     *
+     * @param embedder
+     */
+    public void supportForSingleScenarioExecution(Embedder embedder) {
+        embedder.metaMatchers().put("+scenario_title", new MetaFilter.MetaMatcher() {
+            private String scenarioTitle;
+
+            @Override
+            public void parse(String s) {
+                scenarioTitle = s.substring(16);
+            }
+
+            @Override
+            public boolean match(Meta meta) {
+                // scenarios with examples have no title at the first level of the matcher-tree.
+                if (!meta.hasProperty("title")) {
+                    return true;
+                }
+                return meta.getProperty("title").trim().startsWith(scenarioTitle);
+            }
+        });
     }
 
     public abstract ApplicationContext getApplicationContext();
