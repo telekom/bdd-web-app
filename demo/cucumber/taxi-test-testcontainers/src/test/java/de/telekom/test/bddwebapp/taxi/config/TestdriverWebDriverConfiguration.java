40a65c6925234179932081b003c8f3fc585e07d5
dkeiss <daniel.keiss@gmail.com>
2021-08-29 22:16:53 +0200

    Added testcontainers example

---
+++ demo/cucumber/taxi-test-testcontainers/src/test/java/de/telekom/test/bddwebapp/taxi/config/TestdriverWebDriverConfiguration.java
@@ -0,0 +1,47 @@
+package de.telekom.test.bddwebapp.taxi.config;
+
+import de.telekom.test.bddwebapp.frontend.lifecycle.UsefulWebDriverConfiguration;
+import de.telekom.test.bddwebapp.frontend.lifecycle.WebDriverWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
+
+/**
+ * Overwrite the default configuration and add a new behaviour after browser load.
+ *
+ * @author Daniel Keiss {@literal <daniel.keiss@telekom.de>}
+ * <p>
+ * Copyright (c) 2019 Daniel Keiss, Deutsche Telekom AG
+ * This file is distributed under the conditions of the Apache License, Version 2.0.
+ * For details see the file license on the toplevel.
+ */
+@Component
+@Slf4j
+public class TestdriverWebDriverConfiguration extends UsefulWebDriverConfiguration {
+
+    private final BrowserWebDriverContainer container = new BrowserWebDriverContainer()
+            .withCapabilities(new ChromeOptions())
+            .withRecordingMode(RECORD_ALL, new File("build"));
+
+    @PostConstruct
+    public void overrideDefaultWebDriverConfiguration() {
+        WebDriverWrapper.DEFAULT_WEB_DRIVER_CONFIGURATION = TestdriverWebDriverConfiguration.class;
+    }
+
+    public WebDriver loadWebdriver() {
+        return container.getWebDriver();
+    }
+
+    public BrowserWebDriverContainer getContainer() {
+        return container;
+    }
+}
---
+++ demo/cucumber/taxi-test-testcontainers/src/test/java/de/telekom/test/bddwebapp/taxi/steps/TestcontainersHook.java
@@ -0,0 +1,36 @@
+package de.telekom.test.bddwebapp.taxi.steps;
+
+import de.telekom.test.bddwebapp.taxi.config.TestdriverWebDriverConfiguration;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.lifecycle.TestDescription;
+
+import java.util.Optional;
+
+public class TestcontainersHook {
+
+    @Autowired
+    private TestdriverWebDriverConfiguration webDriverConfig;
+
+    @Before
+    public void beforeScenario() {
+        webDriverConfig.getContainer().start();
+    }
+
+    @After
+    public void afterScenario(Scenario scenario) {
+        webDriverConfig.getContainer().afterTest(new TestDescription() {
+            @Override
+            public String getTestId() {
+                return scenario.getId();
+            }
+            @Override
+            public String getFilesystemFriendlyName() {
+                return scenario.getName();
+            }
+        }, Optional.of(scenario).filter(Scenario::isFailed).map(__ -> new RuntimeException()));
+    }
+
+}
