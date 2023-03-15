6cefc48d6066d663ffacd3892491fed91e4aecda
d.keiss <daniel.keiss@telekom.de>
2023-03-15 10:32:40 +0100

    Remove stale projects

--- demo/cucumber/taxi-test-docker/src/test/java/de/telekom/test/bddwebapp/taxi/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package de.telekom.test.bddwebapp.taxi.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"de.telekom.test.bddwebapp"})
-public class TestConfig {
-}
